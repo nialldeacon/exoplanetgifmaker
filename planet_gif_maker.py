664565cf8b7032fa33b5d23ed8d38b8f96e9dfee
nialldeacon <nialldeacon@hotmail.com>
2022-09-07 19:43:38 +0200

    Add files via upload

--- planet_gif_maker.py
+++ planet_gif_maker.py
@@ -12,11 +12,12 @@ from PIL import Image, ImageDraw, ImageFont
 
 n_slide=5
 today=datetime.date.today()
-indir='~'
-outdir='~'
+indir='/Users/deacon/temp'
+indir1='/Users/deacon/Dropbox/writing/twenty_worlds_promo'
+outdir='/Users/deacon/temp'
 font_loc='/Library/Fonts/HindVadodara-Bold.ttf'
 image_filename=outdir+'/test.gif'
-infile=indir+'/planets_2019.12.15_11.16.29.votable'
+infile=indir+'/planets_2022.09.06_16.08.50.votable'
 factor=0.25
 width=200
 event=['First pulsar planets','First planet orbiting a Sun-like star','First transiting planet','First microlensing planet','CoRoT planet-finding mission launched','First directly-imaged planets orbiting stars','Kepler planet-finding mission launched','First rocky planet around a Sun-like star','Kepler mission finds 715 new exoplanets','Kepler mission finds 1284 new exoplanets','Rocky, temperate TRAPPIST-1 planets','Rocky planet around the nearest star to the Sun','TESS planet-finding mission launched']
@@ -26,30 +27,29 @@ print((event_time[0])[1])
 colours=['orange','green','blue','red','teal']
 months_str=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
 centre_bins=[200,600,1000,1400]
-#load planet images
-earth=Image.open(indir+'/earth.png')
+
+earth=Image.open(indir1+'/earth.png')
 earth.thumbnail((20,20), Image.ANTIALIAS)
-super_earth=Image.open(indir+'/super_earth.png')
+super_earth=Image.open(indir1+'/super_earth.png')
 super_earth.thumbnail((30,30), Image.ANTIALIAS)
-neptune=Image.open(indir+'/neptune.png')
+neptune=Image.open(indir1+'/neptune.png')
 neptune.thumbnail((80,80), Image.ANTIALIAS)
-jup=Image.open(indir+'/jupiter.png')
+jup=Image.open(indir1+'/jupiter.png')
 jup.thumbnail((200,200), Image.ANTIALIAS)
 
 #initialist animated gif frames
 frames = []
 
-#arrange the data into arrays for usage
 table = parse_single_table(infile)
 data=table.array
-date1_bytes=data['pl_publ_date']
+date1_bytes=data['disc_pubdate']
 date1_strings=date1_bytes.astype('U')
 date2_bytes=data['rowupdate']
 date2_strings=date2_bytes.astype('U')
-disc_year=data['pl_disc']
-disc_method_bytes=data['pl_discmethod']
+disc_year=data['disc_year']
+disc_method_bytes=data['discoverymethod']
 disc_method_strings=disc_method_bytes.astype('U')
-pl_hostname_bytes=data['pl_hostname']
+pl_hostname_bytes=data['hostname']
 pl_hostname_strings=pl_hostname_bytes.astype('U')
 pl_letter_bytes=data['pl_letter']
 pl_letter_strings=pl_letter_bytes.astype('U')
@@ -61,6 +61,7 @@ months1= [x[5:7] for x in date1_strings]
 years2 = [x[0:4] for x in date2_strings]
 months2= [x[5:7] for x in date2_strings]
 years=np.zeros(len(years2))
+#print(years2)
 months=np.zeros(len(years2))
 pl_detect_grid=np.zeros((4,5))
 pl_detect_grid_old=np.zeros((4,5))
@@ -68,7 +69,6 @@ pl_disc_method=np.full(len(years2),-1,dtype=int)
 pl_type=np.full(len(years2),-1,dtype=int)
 pl_name=strs = ["" for x in range(len(years2))]
 disc_methods=['Astrometry','Disk Kinematics','Eclipse Timing Variations','Imaging','Microlensing','Orbital Brightness Modulation','Pulsar Timing','Pulsation Timing Variations','Radial Velocity','Transit','Transit Timing Variations']
-#loop over exoplanets sorting their type and discovery methods into numerical categories and parsing their discovery dates 
 for i in range(0,len(years2)):
     index=[j for j, s in enumerate(disc_methods) if disc_method_strings[i] == s]
     #print(pl_hostname_strings[i],pl_letter_strings[i],index)
@@ -84,7 +84,6 @@ for i in range(0,len(years2)):
            pl_type[i]=3
     else:
         if(pl_mass[i]>0.0):
-            #here I'm doing cuts on planets with no radius estimate using the mass estimate
             if((pl_mass[i]>0.0)&(pl_mass[i]<2.0)):
                 pl_type[i]=0
             if((pl_mass[i]>2.0)&(pl_mass[i]<6.0)):
@@ -109,8 +108,8 @@ for i in range(0,len(years2)):
         else:
             years[i]=float(years2[i])
             months[i]=float(months2[i]) 
-#loop through each year and month (exiting at the current date) to make the image frames
-for i0 in range(1990,2021):
+      
+for i0 in range(1990,2023):
     for i1 in range(1,13):
         if(((i0==today.year)&(i1>today.month))|(i0>today.year)):
             break
@@ -211,5 +210,6 @@ for i0 in range(1990,2021):
             frames.append(img)
         #print('overwriting planet_detect_grid_old',i0,i1,i1a,pl_detect_grid_old,pl_detect_grid)
        
-#save the images to an animated GIF
+            
 frames[0].save(image_filename, format='GIF', append_images=frames[1:], save_all=True, duration=50, loop=0)
+print(pl_mass)
